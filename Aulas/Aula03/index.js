cd8483e8a5925c2aaec1bad8fb6449a6ab78515b
leandrafernanda <leandrafriedrich@gmail.com>
2024-08-13 14:05:14 -0300

    Aula 03

---
+++ Aulas/Aula03/index.js
@@ -0,0 +1,61 @@
+// texto exemplo
+//const texto = "O rato roeu a roupa do rei de Roma.";
+/* texto comentário
+linha 1
+linha 2
+linha 3 */
+
+//console.log("Olá Mundo")
+
+//console.log("Olá turma 24-2T")
+
+//console.log("Aprendendo JavaScript")
+
+
+
+/*let nomeVariavel = prompt("Qual seu nome?")
+console.log(typeof nomeVariavel)
+
+let idadeVariavel = prompt("Qual sua idade?")
+console.log(typeof idadeVariavel)*/
+
+/*const nome = "Fulana"
+const idade = 21
+
+console.log("Olá","Meu nome é: ", nome, "e eu  tenho:", idade,"anos");*/
+
+/*const nome = "Leandra"
+const sobreNome = "Friedrich"
+const idade = 40
+const estuda = "true"
+
+ console.log("Meu nome é:",nome, "Seu sobrenome é:",sobreNome, "Sua idade é:", idade, "Estuda no momento?", estuda, "true")*/
+
+ /*const got = "Game of thrones"
+ const temporadasDeGot = "8";*/
+
+ /*console.log(typeof got)
+ console.log(typeof temporadasDeGot)
+
+ let novaVariavel
+ console.log(typeof novaVariavel)
+
+ novaVariavel = 2
+ console.log(typeof novaVariavel)
+
+ novaVariavel = undefined
+ console.log(typeof novaVariavel)*/
+
+
+const idadeNumero = 23
+const idadeTexto = idadeNumero.toString()
+console.log(idadeTexto)
+console.log(typeof idadeNumero)
+console.log(typeof idadetexto)
+
+const idadeTexto2 = "23"
+const idadeNumero2 = Number(idadeTexto2)
+console.log(typeof idadeTexto2)
+console.log(typeof idadeNumero2)
+
+ 
