26bd298bffe13b7e154486ff0eada7db246bb20d
leandrafernanda <leandrafriedrich@gmail.com>
2024-08-15 15:58:34 -0300

    TRabalho de algaritmo

---
+++ Aulas/Aula05/index.js
@@ -0,0 +1,25 @@
+//Variável
+//let a = 10
+//let b = 10
+//imprime a mensagem
+//console.log(b)
+//muda o valor de b para 5
+//b = 5
+//imprime a mensagem
+//console.log(a,b)
+
+/*variável
+let a =10
+let b = 20
+
+atribuir o valor
+c = a
+b = c
+a = b
+imprime a mensagem
+console.log(a,b,c)*/
+
+//variável com impressão da mensagem
+let horasTrabalho = prompt("Quantas horas você trabalha por dia?")
+let valorDia = prompt("Quanto você recebe por dia?")
+console.log("Você recebe" , valorDia, "/" , horasTrabalho, "por hora")
---
+++ Trabalho Validação de algoritmo/Celsius e Fahrenheit/index.js
@@ -0,0 +1,14 @@
+let calculoTemperatura 
+console.log("Converter Celsius e Fahrenheit")
+
+//janela do Alert
+//prompt("Digite seu Nome")
+//prompt("Digite sua idade")
+
+//para digitar a temperatura
+let temperatura = prompt("Digite a temperatura em Celsius:")
+//calculo
+let temperaturaFahrenheit = (temperatura * 9/5) + 32
+//imprime
+console.log(`${temperatura}°C é igual a ${temperaturaFahrenheit}°F`)
+
