cdfabc8e2b20bfaad38c3f067c1c1beeb6c0a493
LeandraFernanda <106930172+LeandraFernanda@users.noreply.github.com>
2024-08-19 17:07:01 -0300

    Add files via upload

---
+++ Aula06/index.js
@@ -0,0 +1,112 @@
+// aula 06 operadores artiméticos
+//exemplo de adição
+/*const primeiroValor = 10
+const segundoValor = 20
+const resultado = primeiroValor + segundoValor + 4
+
+console.log("O resultado é:",resultado)*/
+
+
+//exemplo de subtração
+/*const primeiroValor = 10
+const segundoValor = 20
+const resultado = primeiroValor - segundoValor 
+console.log("O resultado é:",resultado)*/
+
+//exemplo de multiplicação
+/*const primeiroValor = 10
+const segundoValor = 20
+const resultado = primeiroValor * segundoValor
+console.log("O resultado é:",resultado)
+*/
+
+//exemplo de divisão
+/*const primeiroValor = 345
+const segundoValor = 10
+const resultado = primeiroValor / segundoValor
+console.log("O resultado é:",resultado)*/
+
+//exemplo de resto da divisão
+/*const restoDivisao = 11%4
+
+
+console.log("O resultado é:",restoDivisao)*/
+
+
+/*let numero1 = 3
+let numero2 = 4
+let resultado = numero1 + numero2
+console.log("O resultado é:",resultado)*/
+
+/*let numero1 = 3
+let numero2 = 5
+let resultado = numero1 * numero2 /2
+console.log("O resultado é:",resultado)*/
+
+/*let numero1 = 5
+let numero2 = 4
+let resultado = numero1 - numero2 * -1
+console.log("O resultado é:",resultado)*/
+
+/*let numero1 = 234
+let numero2 = 5
+let resultado = numero1 / numero2
+console.log("O resultado é:",resultado)
+const restoDivisao = 234%5
+console.log("O resto da divisão é:",restoDivisao)*/
+
+//comparadores
+// comparador igual
+//comparacao de valores
+/*const condicao = 1 === 1 //resultado true
+console.log("A condição é:",condicao)
+//comparacao de valore tipo igual
+const condicao2 =1 ==="1"
+console.log(condicao2)//resultado false*/
+
+//comparador diferente
+/*const condicao = 1 !== "1"
+console.log("A condição é:",condicao)//resultado true
+const condicao2 =1 !== 1
+console.log("A condicao é:",condicao2)//resultado false
+const condicao3 = 2 !== 1
+console.log("A condição é:",condicao3)//resultado true*/
+
+//comparador maior, maior e igual
+/*const condicao = 1 > 2
+console.log("A condição é:",condicao)//resultado false
+const condicao1 = 2 > 2
+console.log("A condição é:",condicao1)//resultado false
+const condicao2 = 2 >= 2
+console.log("A condição é:",condicao2)//resultado true
+const condicao3 = 3 > 2
+console.log("A condição é:",condicao3)//resultado true*/
+
+//comparador menor, menor e igual
+/*const condicao = 1 < 2
+console.log("A condição é:",condicao)//resultado true
+const condicao1 = 2 < 2
+console.log("A condição é:",condicao1)//resultado false
+const condicao2 = 2 <= 2
+console.log("A condição é:",condicao2)//resultado true
+const condicao3 = 3 < 2
+console.log("A condição é:",condicao3)//resultado false*/
+
+
+
+//Exercícios
+/*const valor1 = 20
+const valor2 = 10
+const compara = valor1 === valor2
+console.log(compara) // resultado false
+
+const compara1 = valor1 === valor2
+console.log(compara) // resultado false
+
+const compara2 = valor1 <= valor2
+console.log(compara2) // resultado false
+
+const compara3 = valor1 >= valor2
+console.log(compara3) // resultado true*/
+
+
