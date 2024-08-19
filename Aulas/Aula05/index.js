16a05fa9f7c364032d45e916fe820bf35a9427db
LeandraFernanda <106930172+LeandraFernanda@users.noreply.github.com>
2024-08-19 14:31:03 -0300

    Add files via upload

--- Aulas/Aula05/index.js
+++ Aulas/Aula05/index.js
@@ -20,6 +20,67 @@ imprime a mensagem
 console.log(a,b,c)*/
 
 //variável com impressão da mensagem
-let horasTrabalho = prompt("Quantas horas você trabalha por dia?")
+/*let horasTrabalho = prompt("Quantas horas você trabalha por dia?")
 let valorDia = prompt("Quanto você recebe por dia?")
-console.log("Você recebe" , valorDia, "/" , horasTrabalho, "por hora")
+console.log("Você recebe" , valorDia, "/" , horasTrabalho, "por hora")*/
+
+
+/*let varNome = prompt("Qual seu nome?")
+console.log(typeof varNome)
+let varIdade = prompt("Qual sua Idade?")
+console.log(typeof varIdade)
+//console.log(varNome, varIdade)
+
+console.log("Olá", varNome, "e você tem", varIdade,"anos")*/
+//Foi impresso: string , string , Seu nome é, Leandra, e você tem 40, anos
+//typeof imprimiu a variável
+
+/*let varResposta1
+let varResposta2
+let varResposta3
+//atribui com impressão
+varResposta1=prompt("Você gosta do mar?")
+console.log(typeof varResposta1)
+//prompt é uma função que pede uma resposta ao usuário
+varResposta2=prompt("tem interesse em sair do país?")
+console.log(typeof varResposta2)
+//prompt é uma função que pede uma resposta ao usuário
+varResposta3=prompt("Você gosta de viajar?")
+console.log(typeof varResposta3)
+//imprime a frase com as respostas
+ console.log("", varResposta1, "claro que", varResposta2, "acho que",varResposta3 )*/
+
+//variavel
+ //let a = 10
+ //let b = 25
+ //let c 
+// trocar os valores
+//c = a
+//a = b
+//b = c
+//imprime 25, 10
+//console.log(a,b) 
+//console.log(b,a)
+//Depois de trocados, o resultado
+//console.log("O valor de a é", a)//o novo valor de a é 25
+//console.log("O valor de b é", b)//o novo valor de b é 10
+
+
+//let nome;
+//let Idade;
+
+//nome = prompt("Qual seu nome?");
+//idade = prompt("Qual sua idade?");
+//console.log(typeof nome, typeof idade);
+
+//console.log("Olá",nome,"você tem",idade,"anos");
+
+
+
+
+
+
+
+
+
+
