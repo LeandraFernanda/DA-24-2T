944a3587d6ad01eb329eb90ff0491d68ef77764d
LeandraFernanda <106930172+LeandraFernanda@users.noreply.github.com>
2024-08-22 17:29:35 -0300

    Add files via upload

---
+++ Aulas/Aula08/index.js
@@ -0,0 +1,80 @@
+/*const text1 = "Olá Mundo"
+const texto2 = `Olá Mundo`
+const texto3 = "Aqui programando!"
+
+const nome = "Leandra"
+const idade = 40*/
+//exemplo de comcatenação
+//const frase  = "O meu nome é:"
+//+ nome + "e minha idade é:" + idade + "anos"
+
+/*const frase = `O meu nome é: ${nome} e minha  idade é: ${idade} anos`
+console.log(frase)*/
+
+/*const usuario = prompt("Digite seu nome")
+const corFavorita = pronpt("Digite sua cor favorita")
+const frase = `Meu nome: ${usuario} e minha cor favorita é ${corFavorita} `
+console.log(frase)*/
+
+/*const nome = "Leandra"
+console.log(nome.length)
+console.log(nome.toLowerCase())
+console.log(nome.toUpperCase())*/
+
+/*const email = "leandrafriedrich@gmail.com"
+console.log(email.trim())
+
+const frase ="Hoje comi cenoura"
+console.log(frase.includes("cenoura"))
+console.log(frase.includes("batata"))*/
+
+/*const frase = "Hoje comi cenoura e adoro cenoura"
+const novaFrase = frase.replaceAll("cenoura","batata")
+//ele inverte cenoura pra batata***
+console.log(novaFrase)*/
+
+/*const frase = prompt("Digite uma frase:")
+console.log(frase)
+console.log(frase.includes("o"))
+console.log(frase.includes("a"))
+console.log(frase.toLocaleUpperCase())*/
+
+// Array de frutas
+/*const frutas = ["Abacate", "Banana", "Tomate", "Milho", "Uva","Morango"]
+const fruta = frutas[4]
+console.log(fruta)*/
+
+/*const racas = ["rnd","puldoo","rotwaler","chiuaua"]
+const raca = Number(prompt("Digite um numero até 4"))
+console.log(raca)*/
+
+/*const pokemons = ["bubasauro","squirtle","charmame","pikachu"]
+console.log(pokemons.length)*/
+
+/*const seriesBoas = ["Vikins", "The Big Bang Theory"]
+console.log(seriesBoas.includes("Vikins"))
+console.log(seriesBoas.includes("The Game of Thrones"))//true e false */
+
+/*const numeros = [1,2,3]
+numeros.push(4)
+console.log(numeros)//[1,2,3]
+numeros.push(5,6,7)
+console.log(numeros)//[1,2,3,4,5,6,7 */
+
+/*const meusPeixes = ["Palhaço","mandarin","estorjão"]
+meusPeixes.pop()
+console.log(meusPeixes)//["Palhaço","mandarin"] */
+
+/*const letras = ["A","B","C","D","E","F","G","H"]
+letras.splice(2,1)
+console.log(letras)
+letras.splice(5,2)*/
+
+const numeros = ["1","2","3","4","5","6"]
+console.log(numeros.push("7"))
+numeros.splice(3,2)
+console.log(numeros)
+console.log(numeros.length)
+
+
+
