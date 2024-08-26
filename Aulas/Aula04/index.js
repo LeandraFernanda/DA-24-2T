4a31e9f34bad3a1dbccc9f19aed3888b6d2d8e84
LeandraFernanda <106930172+LeandraFernanda@users.noreply.github.com>
2024-08-26 15:37:03 -0300

    Add files via upload

---
+++ Aulas/Aula04/index.js
@@ -0,0 +1,25 @@
+// Tipos de variáveis
+
+/*let nome = "Le"
+let sobrenome = "F"
+let idade = 40
+let estudante = true
+console.log("Nome:", nome)
+console.log("Idade:", idade)
+console.log("Estudante:", estudante)*/  
+
+
+
+
+/*const nome = prompt("Qual seu nome?")
+const idade = prompt("Qual sua idade?")
+console.log("Olá",nome,"você tem",idade,"anos")
+console.log(typeof nome,idade)*/
+
+
+
+
+
+
+
+ 
