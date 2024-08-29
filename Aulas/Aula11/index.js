be1098591225c0e3f20c65fa2ba85eb8254df97c
LeandraFernanda <106930172+LeandraFernanda@users.noreply.github.com>
2024-08-29 17:10:39 -0300

    Add files via upload

---
+++ Aulas/Aula11/index.js
@@ -0,0 +1,107 @@
+/*const respostaDoUsuario = prompt("Digite o número que você quer testar")
+const numero =  Number(respostaDoUsuario)
+
+if(numero % 2 === 0){
+    console.log("Passou no teste!")
+} else {
+    console.log("Não passou no teste!")
+}*/
+
+/*const escolhaDoUsuario = prompt("Escolha Pedra, Papel, Tesoura, Lagarto ou Spock:").toLowerCase();
+switch(escolhaDoUsuario){
+    case 'pedra':
+        console.log("Você escolheu pedra");
+        break;
+    case 'papel':
+        console.log("Você escolheu papel");
+        break;
+    case 'tesoura':
+        console.log("Você escolheu tesoura");
+        break;
+    case 'lagarto':
+        console.log("Você escolheu lagarto");
+        break;
+    case'spock':
+        console.log("Você escolheu spock");
+        break;
+    default:
+        console.log("Escolha inválida. Escolha entre pedra, papel, tesoura, lagarto ou spock.")
+        break;
+}
+
+let choices = []
+const escolhaComputador = choices[Math.floor(Math.random()*5)];
+console.log(escolhaComputador);*/
+
+/*let fruta = prompt("Escolha uma fruta:")
+let preco 
+switch(fruta){
+    case 'Laranja':
+        preco = 3.5
+        break;
+    case 'Maçã':
+        preco = 2.25
+        break;
+    case 'Uva':
+        preco = 0.30
+        break;
+    case 'Pêra':
+        preco = 5.5
+        break;//Break para o item c.
+    default:
+        preco = 5
+        break;
+}
+console.log("o preço da fruta",fruta,"R$",preco)*/
+
+/*const numero = Number(prompt("Digite o primeiro número:"))
+if(numero > 0) {
+    console.log("Esse número passou no teste")
+let mensagem = prompt("Essa mensagem é segreta!!")
+}*/
+
+//perguntar ao usuario qual a idade e imprima,,,, só maiores de 18
+/*let idade = Number(prompt("Qual sua idade?"))
+
+if(idade > 18) {
+    console.log("Você é maior de 18 anos, pode dirigir!")
+} else {
+    console.log("Você não é maior de 18 anos,você não pode dirigir! ")
+}
+console.log(idade)*/
+
+//verificar o turno do dia de um aluno que estuda M(matutino), V(Vespertino)ou N(Noturno)
+
+/*let turnoDoAluno = prompt("Digite seu turno Aluno: M(Matutino), V(vespertino) ou N(Noturno)")
+switch(turnoDoAluno){
+    case 'M':
+        console.log("Turno Matutino, bom Dia aluno!!")
+        break;
+    case 'V':
+        console.log("Turno Vespertino, boa tarde aluno!!")
+        break;
+    case 'N':
+        console.log("Turno Noturno, boa noite aluno!!")
+        break;
+    default:
+        console.log("Turno inválido")
+        break;
+}
+console.log(turnoDoAluno)*/
+
+
+//fazer um programa que pergunte ao uruário qual o genero do filme quer ver e o preço do ingresso, perguntar para o usuario se vai assistir o filme e imprimir Bom filme, senão, imprima outro filme.
+/*let generoDoFilme = prompt("Qual o genero do filme que você quer assistir?")
+
+let precoDoIngresso = Number(prompt("Qual o preço do ingresso?"))
+
+let vaiAssistir = prompt("Vai assistir o filme? (sim ou não)").toLowerCase()    
+
+if(vaiAssistir === "sim"){
+    console.log("Bom filme!")
+} else {
+    console.log("Outro filme.")
+}
+
+console.log(generoDoFilme, precoDoIngresso, vaiAssistir)*/
+
