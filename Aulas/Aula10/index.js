c3676ea853b68ed85e207852252c71ea944fe0e4
LeandraFernanda <106930172+LeandraFernanda@users.noreply.github.com>
2024-08-29 17:10:06 -0300

    Add files via upload

---
+++ Aulas/Aula10/index.js
@@ -0,0 +1,128 @@
+/*let condicao1 = true
+if(condicao1){
+    //ação se a condição for verdadeira
+    console.log("Entrou no if 1")
+}
+
+let condicao2 = false
+if(condicao2){
+    //ação se a condição for falso
+    console.log("Entrou no if 2")
+}*/
+
+//condição
+/*const num1 = Number(prompt("Digitenumero 1"))
+const num2 = Number(prompt("Digite o numero 2"))
+num1 === num2
+if(num1 === num2){
+    console.log("Sucesso")
+}*/
+
+/*const num1 = Number(prompt("Digite o numero1"))
+const num2 = Number(prompt("Digite o numero2"))
+num1 === num2
+if(num1 === num2){console.log("Os numeros são iguais!")}
+else if(num1>num2){console.log("O num1 é maior que o numn2")}
+else{console.log("O num1 é menor que o num2")}*/
+
+/*let paisDeOrigem = prompt("Digite seu páis de origem:")
+
+switch(paisDeOrigem){
+    case "Brasil":
+        console.log("Você é brasileiro!")
+        break;
+    case "Argentina":
+        console.log("Você é argentino!")
+        break;
+    case "Chile":
+        console.log("Você é chileno!")
+        break;
+    case "EUA":
+        console.log("Você é americano!")
+        break;
+    case "Peru":
+        console.log("Você é peruano!")
+        break;
+    case "Inglaterra":
+        console.log("Você é inglês!")
+        break;
+    case "Japão":
+        console.log("Você é japonês!")
+        break;
+    case "Canadá":
+        console.log("Você é canadense!")
+        break;
+    case "México":
+        console.log("Você é mexicano!")
+        break;
+    case "Uruguay":
+        console.log("Você é uruguayo!")
+        break;
+    case "Portugal":
+        console.log("Você é português!")
+        break;
+    default:
+        console.log("Você não é brasileiro, argentino ou chileno!")
+}*/
+
+/*const pokemon = prompt("Qual o popkemoin?")
+switch(pokemon){
+    case `Bulsasaur`:
+        console.log("Bulbasaur possui 75 pontos de vida.")
+        break;
+    case `Charmander`:
+        console.log("Charmander possui 65 pontos de vida.")
+        break;
+    case `Squirtle`:
+        console.log("Squirtle possui 44 pontos de vida.")
+        break;
+    case `Pikachu`:
+        console.log("Pikachu possui 35 pontos de vida.")
+        break;
+    case `Eevee`:
+        console.log("Eevee possui 55 pontos de vida.")
+        break;
+    case `Squirtle`:
+        console.log("Squirtle possui 44 pontos de vida.")
+        break;    
+    default:
+        console.log("Esse Pokémon não existe!")
+}*/
+
+/*const harryPotter = prompt("Qual casa de Hogwarts o chapéu vai colocar")
+switch(harryPotter){
+    case `Herminione(Grfinória)`:
+        console.log("Você colocará o chapéu em Grifinória.")
+        break;
+    case `Ron(Slytherin)`:
+        console.log("Você colocará o chapéu em Slytherin.")
+        break;
+    case `Harry(Gryffindor)`:
+        console.log("Você colocará o chapéu em Gryffindor.")
+        break;
+    case `Ana(Lufa-lufa)`:
+        console.log("Você colocará o chapéu em Lufa-Lufa.")
+        break;
+    case `Luna(Corvinal)`:
+        console.log("Você colocará o chapéu em Corvinal.")
+        break;
+    case `Narcisa(Sonserina)`:
+        console.log("Você colocará o chapéu em Sonserina.")
+        break;    
+    default:
+        console.log("Esse casa de Hogwarts não existe!")
+}*/
+
+
+/*const ensino = prompt("Concluiu o ensino médio?"). toLowerCase() === `sim`
+const idade = Number(prompt("Digite sua idade:")) >=18
+const faculdade = prompt("Você faz Faculdade?").toLowerCase()===`sim`
+
+if(ensino && idade && faculdade){
+    console.log("Você pode se candidatar ao curso de Tecnologia da Informação.")
+}else{
+    console.log("Você não pode estudar nessa Faculdade.")
+}*/
+
+//fazer o desafio
+
