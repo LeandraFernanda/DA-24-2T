bde848c3769776f8d6ccb117f48d49b7bd4523d9
LeandraFernanda <106930172+LeandraFernanda@users.noreply.github.com>
2024-09-17 13:38:23 -0300

    Add files via upload

---
+++ Aulas/Aula14/index.js
@@ -0,0 +1,40 @@
+/*Contador Simples:
+Escreva um programa que use um loop for para contar de 1 a 10 e exibir cada número no
+console.*/
+/*for (let i = 1; i < 10; i++){
+    console.log(i);
+}*/
+
+/*Contador de Pares:
+Modifique o programa anterior para contar apenas os números pares de 1 a 20.*/
+
+/*for (let i = 1; i <= 20; i++)
+    if (i % 2 === 0) {
+        console.log(i);
+    }*/
+
+/*Tabuada:
+Peça ao usuário um número usando prompt e exiba a tabuada desse número de 1 a 10
+usando um loop for.*/
+
+/*let numero = Number(prompt("Digite um número de 1 á 10 da tabuada:"));
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${numero} x ${i} = ${numero * i}`);
+}*/
+
+/*Soma de Números:
+Peça ao usuário para inserir 5 números. Use um loop for para somar esses números e exiba
+o total.*/
+/*let soma = [];
+for (let i = 0; i< numeros.length; i++){
+    soma += numeros[i];
+}
+
+console.log("A soma dos números é:", soma);*/
+
+
+
+
+    
+
