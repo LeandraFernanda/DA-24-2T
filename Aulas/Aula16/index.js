e12b7118ce23265963f1d1b6e925b2754f1ee71b
LeandraFernanda <106930172+LeandraFernanda@users.noreply.github.com>
2024-09-17 13:39:34 -0300

    Add files via upload

---
+++ Aulas/Aula16/index.js
@@ -0,0 +1,80 @@
+/*function minhaFuncao(variavel){
+    return variavel * 5
+}*/
+
+//console.log(minhaFuncao(2))10
+//console.log(minhaFuncao(10))50
+// Se tirar o console.log,Não aparece nada
+
+/*let textoDoUsuario = prompt("Insira um texto:");//declara uma variavel e aparece uma caixa de dialogo
+const outraFuncao = function(texto){//declara uma constante
+    return texto.toLowercase().includes("Cenoura")//converte o texto de minusculo para maiusculo
+}
+const resposta = outraFuncao(textoDoUsuario)//ele vê se tem a palavra cenoura no texto 
+console.log(resposta)*/
+
+
+/*function sobreMim(nome, idade, cidade, estudante) {
+    let estudanteStatus = estudante? "sim" : "não";
+    return `Olá! Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}. Sou estudante: ${estudanteStatus}!`;
+}
+
+console.log(sobreMim("Leandra", 40, "São Paulo", true));*/
+
+
+/*function somar(numero1, numero2) {
+    const resultado = numero1 + numero2; 
+    return resultado
+  }
+  const resultadoDaSoma = somar(5, 3)
+  console.log("A soma é:", resultadoDaSoma)*/
+
+
+
+  /*function maiorOuIgual(numero1,numero2){
+    return numero1 += numero2;
+  }
+  console.log(maiorOuIgual(2,3))  
+  console.log(maiorOuIgual(3,5))  
+  console.log(maiorOuIgual(5,7)) 
+  
+  function Par(numero){
+    return numero % 2 === 0;
+  }
+  console.log(Par(2))   
+  console.log(Par(3))  
+  console.log(Par(1)) 
+  function escreverMensagem(mensagem){
+  
+    const tamanho = mensagem.length
+  
+    const mensagemMaiuscula = mensagem.toUpperCase()
+    console.log("Tamanho da mensagem:",tamanho)
+    console.log("Mensagem em maiúsculas:",mensagemMaiuscula)
+  }
+  escreverMensagem("Olá mundo!, aqui Leandra programando no Senac de São Leopoldo.")*/  
+
+
+  
+  /*function somar(numero1,numero2){
+    return numero1+numero2
+  };
+  function subtrair(numero1,numero2){
+    return numero1-numero2
+  };
+  
+  function multiplicar(numero1,numero2){
+    return numero1*numero2
+  };
+  function dividir(numero1,numero2){
+    return numero1/numero2
+  };
+  const numero1 = parseFloat(prompt("Digite o primeiro número:"))
+  const numero2 = parseFloat(prompt("Digite o segundo número:"))
+  console.log("A soma é:", somar(numero1,numero2))
+  console.log("A subtração é:", subtrair(numero1,numero2))
+  console.log("A multiplicação é:", multiplicar(numero1,numero2))
+  console.log("A divisão é:", dividir(numero1,numero2))*/
+
+
+  
