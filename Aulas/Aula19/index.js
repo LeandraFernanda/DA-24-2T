8180a8c140e6c91ecf58a53c6ca56f86ac634695
LeandraFernanda <106930172+LeandraFernanda@users.noreply.github.com>
2024-09-23 15:07:52 -0300

    Add files via upload

---
+++ Aulas/Aula19/index.js
@@ -0,0 +1,129 @@
+/*function minhaFuncao(objeto,propriedade){
+    return objeto[propriedade]
+}
+
+const pessoa = {
+    nome:"Caio",
+    idade:23,
+    backender:false
+}
+
+console.log(minhaFuncao(pessoa,"backender"))
+
+console.log(minhaFuncao(pessoa,"altura"))*/
+
+
+
+
+/*function imprimeApelidos(objeto) {  
+    console.log(`Olá, meu nome é ${objeto.nome} e meus apelidos são ${objeto.apelidos[0]}, ${objeto.apelidos[1]}, ${objeto.apelidos[2]}.`)
+}*/
+
+
+/*const pessoa = {
+    nome: "Lucas",
+    apelidos: ["Mestre", "Prof", "Lucas"]
+  };
+  
+  function imprimirMenssagem(objeto) {
+    console.log(`Eu sou ${objeto.nome}, mas pode me chamar de: ${objeto.apelidos[0]}, ${objeto.apelidos[1]} ou ${objeto.apelidos[2]}.`);
+  }
+  
+  imprimirMenssagem(pessoa);*/
+
+
+
+
+
+  /*const pessoa = {
+    nome: "Lucas",
+    apelidos: ["Mestre", "Prof", "Lucas"]
+  };
+  console.log(`Eu sou ${objeto.nome}, mas pode me chamar de: ${objeto.apelidos[0]}, ${objeto.apelidos[1]} ou ${objeto.apelidos[2]}.`);/*
+
+
+
+
+/*function criarArray(objeto) {
+    const array = [objeto.nome, objeto.nome.length, objeto.idade, objeto.profissão, objeto.profissão.length];
+    return array;
+}
+
+const pessoa1 = {
+    nome: "Lucas",
+    idade: 25,
+    profissão: "Desenvolvedor Front-end"
+};
+
+const pessoa2 = {
+    nome: "Maria",
+    idade: 30,
+    profissão: "Designer Gráfica"
+    };
+    
+console.log(criarArray(pessoa1));
+
+console.log(criarArray(pessoa2));*/
+
+
+/*const pessoa1 = {
+    nome:"Maria",
+    idade:25,
+    profissão:"Desenvolvedora"
+};
+const pessoa2 = {
+    nome:"João",
+    idade:32,
+    profissão:"Designer"
+};
+function retornarInformacoes(objeto){
+    const arrayPessoa = [
+        objeto.nome,
+        objeto.nome.length
+        objeto.idade,
+        objeto.profissão,
+        objeto.profissão.length
+    ]
+    return arrayPessoa;
+}
+console.log(retornarInformacoes(pessoa1));
+console.log(retornarInformacoes(pessoa2));*/
+
+
+
+/*
+let carrinho = [];
+
+const fruta1 = {
+    nome: "Maçã",
+    disponibilidade: true
+};
+
+const fruta2 = {
+    nome: "Laranja",
+    disponibilidade: true
+};
+
+const fruta3 = {
+    nome: "Uva",
+    disponibilidade: true
+};
+
+function adicionarFrutaAoCarrinho(fruta) {
+    carrinho.push(fruta);
+}
+
+adicionarFrutaAoCarrinho(carrinho);
+
+*/
+
+
+
+
+
+
+
+
+
+
+
